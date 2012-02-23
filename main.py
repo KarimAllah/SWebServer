59dcabcd550019512da51458b3a8ef5f9fc6a152
Karim Allah Ahmed <karim.allah.ahmed@gmail.com>
2012-02-23 04:14:29 +0200

    Serving simple static HTML page

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import socket
 import logging
 
@@ -7,8 +6,12 @@ import config
 
 logger = logging.getLogger(config.SERVER_NAME)
 
+STATIC_PAGE = '''<html><head><title>TEST</title></head><body>NULL</body></html>'''
+
+HTTP_PAGE = 'HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: %s\r\n\r\n%s' % (len(STATIC_PAGE), STATIC_PAGE)
+
 def handle_connection(conn, addr):
-	conn.sendall("SimpleTCPServer")
+	conn.sendall(HTTP_PAGE)
 
 def main():
 	logger.debug("Launching ( %s ) on ( %s, %s )", config.SERVER_NAME, config.HOST, config.PORT)
@@ -26,9 +29,9 @@ def main():
 			if not pid:
 				handle_connection(conn, addr)
 				conn.close()
-				sys.exit(0)
+				break
 	except KeyboardInterrupt:
-		logger.debug("Normally exiting ( %s )", config.SERVER_NAME)
+		logger.debug("Normally exiting.")
 	except Exception as ex:
 		logger.debug("Uncaught exception raised, (%s)", ex)
 	finally:
